644acb730aff21891685ac527abbe02692574318
Jay <patej13@farmingdale.edu>
2024-08-30 21:48:42 -0400

    Code

---
+++ app/src/main/java/com/example/bcs371_helloworld_jaypatel/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.bcs371_helloworld_jaypatel
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.bcs371_helloworld_jaypatel.ui.theme.BCS371_HelloWorld_JayPatelTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            BCS371_HelloWorld_JayPatelTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "World",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    BCS371_HelloWorld_JayPatelTheme {
+        Greeting("World")
+    }
+}
